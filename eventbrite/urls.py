59436e6869e0a686b9f4ad445b10a5e475c6bf30
andyyu <andyhsyu@gmail.com>
2016-01-30 21:54:46 -0800

    index

--- eventbrite/urls.py
+++ eventbrite/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
